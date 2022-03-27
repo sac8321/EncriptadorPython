e9565396bd4765403bbdef808ecab7708a804b23
sac8321 <sac8321@gmail.com>
2022-03-26 19:51:23 -0700

    main add

---
+++ main.py
@@ -0,0 +1,48 @@
+from cgitb import text
+
+
+def encriptar(texto):
+    print ('Funcion para encriptar texto '+texto)
+    textoFinal=''
+    for letra in texto:
+        textoFinal += letra+'x'
+    return textoFinal
+
+def desencriptar(texto):
+    print('Funcion para desencriptar '+texto)
+    textoFinal=''
+    contador=0
+    for letra in texto:
+        if contador %2 ==0 :         
+            textoFinal += letra
+        contador +=1
+    return textoFinal
+
+def encriptarArchivo(rutaArchivo):
+    archivo = open(rutaArchivo,'r')
+    texto = archivo.read()
+    archivo.close()
+    textoEncriptado = encriptar(texto)
+    
+    archivo = open(rutaArchivo,'w')
+    archivo.write(textoEncriptado)
+    archivo.close()
+
+
+def desencriptarArchivo(rutaArchivo):
+    archivo = open(rutaArchivo,'r')
+    texto = archivo.read()
+    archivo.close()
+    textoDesEncriptado = desencriptar(texto)
+    
+    archivo = open(rutaArchivo,'w')
+    archivo.write(textoDesEncriptado)
+    archivo.close()
+    
+respuestaEoD = input('Presione E para encriptar o D para desencriptar')
+rutaArchivo = input('Ingrese la ruta del archivo')
+
+if respuestaEoD == 'E':
+    encriptarArchivo(rutaArchivo)
+else:
+    desencriptarArchivo(rutaArchivo)
